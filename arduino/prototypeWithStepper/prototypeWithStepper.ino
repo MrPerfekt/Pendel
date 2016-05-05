7703a9826a0bd1b056ddab08cbfe985b577edf4d
Andreas Gruber <grubi@edumail.at>
2016-05-05 20:36:58 +0200

    Implement KnobEncoder

--- arduino/prototypeWithStepper/prototypeWithStepper.ino
+++ arduino/prototypeWithStepper/prototypeWithStepper.ino
@@ -1,5 +1,8 @@
 //#include<Servo.h>
 
+//==Laser==
+const uint8_t pinLaser = 11;
+
 //==LEDs==
 const uint8_t ledCnt = 9;
 const uint8_t ledPins[ledCnt]={43,45,47,49,51,53,48,50,52};
@@ -10,10 +13,19 @@ const uint8_t pinEncoderA=2;
 const uint8_t pinEncoderB=3;
 unsigned long lastEncoderTime = 0;
 unsigned long lastEncoderTimeDiff;
+int encoderPos=0;
+
+//==KnobEncoder==
+const uint8_t knobCnt = 2;
+const uint8_t knobEncoderPin[knobCnt][2]={{28,26},{24,22}};
+uint8_t knobEncoderLastState[knobCnt][2]={{0,0},{0,0}};
+int32_t knobEncoderPos[knobCnt];
 
 ////==Servo==
 //const uint8_t pinServoV=8;
 //const uint8_t pinServoH=9;
+//Servo servoH;
+//Servo servoV;
 
 //==Stepper==
 const int stepperPinCnt = 4;
@@ -29,21 +41,27 @@ const uint8_t fsmStepper[stepperStateCnt][stepperPinCnt]={
   {0,0,0,1},
   {1,0,0,1}
 };
+int stepperPos[2]={0,0};
+int stepperPosDelta[2]={0,0};
 
 //==Magnet==
-const uint8_t pinMagnet[2] = {44,42};//{48,46}{44,42};
+const uint8_t pinMagnet[2] = {46,44};
+const uint8_t pinMagnetPwm = 10;
 const uint8_t range = 20;
-
-//==Encoder==
-int encoderPos=0;
-////==Servo==
-//Servo servoH;
-//Servo servoV;
-
-//==Stepper==
-int stepperPos[2]={0,0};
   
 void setup() {
+  //==Laser==
+  pinMode(pinLaser,OUTPUT);
+  analogWrite(pinLaser,127);
+   TCCR1B = (TCCR1B & ~0b111) | 2;
+  //Setting   Divisor   Frequency
+  //0x01    1     31372.55
+  //0x02    8     3921.16
+  //0x03      64    490.20   <--DEFAULT
+  //0x04      256     122.55
+  //0x05    1024    30.64
+  //TCCR1B = (TCCR1B & 0b11111000) | <setting>;
+  
   //==LEDs==
   for(uint8_t i = 0; i < ledCnt;i++){
     pinMode(ledPins[i], OUTPUT);
@@ -63,9 +81,29 @@ void setup() {
   //==Magnet==
   pinMode(pinMagnet[0], OUTPUT);
   pinMode(pinMagnet[1], OUTPUT);
-  //setMagnet(true,false); //Push
-  //delay(500);
+  pinMode(pinMagnetPwm, OUTPUT);
+  TCCR2B = (TCCR2B & 0b11111000) | 1;
+  /*
+  Setting   Divisor   Frequency
+  0x01    1     31372.55
+  0x02    8     3921.16
+  0x03      64    490.20   <--DEFAULT
+  0x04      256     122.55
+  0x05    1024    30.64
+  TCCR1B = (TCCR1B & 0b11111000) | <setting>;
+  */
 
+  /*
+  //Test Magnet Code
+  //digitalWrite(pinMagnetPwm,HIGH);
+  while(1){
+    setMagnet(true,false,200); //Push
+    delay(1000);
+    setMagnet(true,true,200); //Push
+    delay(1000);
+  }
+  */
+  
   /*
   //==Example Stepper Code==
   for(int i = 0; i < stepperPinCnt; i++){
@@ -95,14 +133,16 @@ void setup() {
 }
 
 //==Magnet==
-void setMagnet(bool on, bool pull){
+void setMagnet(bool on, bool pull, uint8_t speed){
     digitalWrite(pinMagnet[0],on && !pull);
     digitalWrite(pinMagnet[1],on && pull);
+    analogWrite(pinMagnetPwm,speed);
 }
 
 //==Stepper==
 void updateStepper(uint8_t stepper){
-  int stepperState = stepperPos[stepper] % stepperStateCnt;
+  int32_t pos = stepperPos[stepper] + stepperPosDelta[stepper];
+  int stepperState = pos % stepperStateCnt;
   if(stepperState < 0){
     stepperState += stepperStateCnt;
   }
@@ -112,7 +152,14 @@ void updateStepper(uint8_t stepper){
   }
 }
 
+uint32_t loopCnt = 0;
 void loop(){
+  /*
+  (Serial.available() >= 4){
+    char dir = Serial.parseInt();
+    int pos
+  }
+  */
   /*
   //Serial.println(position);
   //Length Pendulum:175mm
@@ -126,7 +173,34 @@ void loop(){
 
   updateStepper(0);
   updateStepper(1);
+  loopCnt++;
+  if(loopCnt % 10 == 0){
+    updateKnobEncoder(0);
+    updateKnobEncoder(1);
+    //if(knobEncoderPos[0] != 0){
+    //  Serial.println(knobEncoderPos[0]);
+    //}
+    //Serial.println(knobEncoderPos[1]);
+    stepperPosDelta[0] += knobEncoderPos[0];
+    stepperPosDelta[1] += knobEncoderPos[1];
+    knobEncoderPos[0] = 0;
+    knobEncoderPos[1] = 0;
+  }
   //Serial.println(encoderPos);
+}
+
+void updateKnobEncoder(uint8_t nr){
+  uint8_t lasta = knobEncoderLastState[nr][0];
+  uint8_t lastb = knobEncoderLastState[nr][1];
+  uint8_t ina = digitalRead(knobEncoderPin[nr][0])==HIGH;
+  uint8_t inb = digitalRead(knobEncoderPin[nr][1])==HIGH;
+  if(ina==lasta && inb==lastb){
+    return;
+  }
+  //if(ina != lasta || inb != lastb)
+  knobEncoderPos[nr]+=fsmEncoder[(ina<<1)+inb][(lasta<<1)+lastb];
+  knobEncoderLastState[nr][0] = ina;
+  knobEncoderLastState[nr][1] = inb;
 }
 
 void incrementEncoder(){
@@ -138,11 +212,12 @@ void incrementEncoder(){
   encoderPos+=fsmEncoder[(ina<<1)+inb][(lasta<<1)+lastb];
   lasta = ina;
   lastb = inb;
+  
   unsigned long newTime = micros();
   lastEncoderTimeDiff = newTime - lastEncoderTime;
   lastEncoderTime = newTime;
-  stepperPos[0]=-(int)(encoderPos*1.5);
-  stepperPos[1]= -abs((int)(encoderPos/2));
+  stepperPos[0]=-(int)(encoderPos/2);
+  stepperPos[1]= -abs((int)(encoderPos/4));
 
   for(uint8_t i = 0; i < ledCnt;i++){
     digitalWrite(ledPins[i],(ledCnt-i)*5 > abs(encoderPos));
@@ -154,26 +229,21 @@ void incrementEncoder(){
   if(abs(encoderPos) > range){
     if(magnetOn){
       magnetOn = 0;
-      setMagnet(false,false); //off
+      setMagnet(false,false,0); //off
       maximum = 0;
-      Serial.println("a");
     } else {
       if(abs(maximum) < abs(encoderPos)){
         maximum = encoderPos;
-        Serial.println("a2");
-        Serial.println(maximum);
       }
     }
   } else {
     if(!magnetOn){
       magnetOn = 1;
-      setMagnet(1,true);
+      setMagnet(1,true,176);
       magnetDir = true;
-      Serial.println("b");
     } else if(((encoderPos < 0) != (maximum < 0)) && magnetDir){
       magnetDir = false;
-      setMagnet(0,false);
-      Serial.println("c");
+      setMagnet(0,false,0);
     }
   }
 }
